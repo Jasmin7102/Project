509e7e8da4d474cd6695c846099a3bffe0265b6a
TO THE NEW <jasmineanand1999@gmail.com>
2022-10-14 11:30:39 +0530

    Pushing code on OTP_Screen branch

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Screen from './Screen';
+//import App from './App';
+//import Screen from './Screen';
+import OtpScreen from './OtpScreen';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Screen);
+AppRegistry.registerComponent(appName, () => OtpScreen);
