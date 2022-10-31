7474f162f2ae86db1d4203306037db396bceca1f
TO THE NEW <jasmineanand1999@gmail.com>
2022-10-31 23:54:42 +0530

    animation updated

--- App.js
+++ App.js
@@ -1,161 +1,79 @@
-import React, { useEffect, useRef } from 'react';
-import { Animated, Easing, Dimensions, SafeAreaView } from 'react-native';
+import React, { useEffect, useRef } from 'react'
+import { StyleSheet, View, Animated, Dimensions } from 'react-native'
 
-console.log('Dimensions', Dimensions.get('screen').height);
+const Item = (props) => {
+    const textSize = useRef(new Animated.Value(0)).current;
+    const spinValue = useRef(new Animated.Value(0)).current;
+    const cordinateX = useRef(new Animated.Value(0)).current;
+    const cordinateY = useRef(new Animated.Value(0)).current;
+    const textOpacity = useRef(new Animated.Value(0)).current;
 
-export default App = () => {
-
-  const coordinateXFirst = useRef(
-    new Animated.Value(0)
-  ).current;
-
-  const coordinateYFirst = useRef(
-    new Animated.Value(0)
-  ).current;
-
-  const coordinateXSecond = useRef(
-    new Animated.Value(0)
-  ).current;
-
-  const coordinateYSecond = useRef(
-    new Animated.Value(0)
-  ).current;
-
-  const coordinateXThird = useRef(
-    new Animated.Value(0)
-  ).current;
-
-  const coordinateYThird = useRef(
-    new Animated.Value(Dimensions.get('window').height)
-  ).current;
-
-  const coordinateXFourth = useRef(
-    new Animated.Value(Dimensions.get('window').width)
-  ).current;
-
-  const coordinateYFourth = useRef(
-    new Animated.Value(Dimensions.get('window').height)
-  ).current;
-
-  const sizeValue =
-    useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-
-    Animated.loop(
-      Animated.timing(sizeValue, {
-        toValue: 80,
-        duration: 8000,
-        useNativeDriver: false,
-      }),
-    ).start();
-
-    Animated.loop(
-      Animated.timing(coordinateXFirst, {
-        duration: 8000,
-        toValue: Dimensions.get('window').width - 180,
-        useNativeDriver: false,
-      }),
-    ).start();
-
-    Animated.loop(
-      Animated.timing(coordinateYFirst, {
-        duration: 8000,
-        toValue: Dimensions.get('window').height - 200,
-        useNativeDriver: false,
-      }),
-    ).start();
-
-    Animated.loop(
-      Animated.timing(coordinateXSecond, {
-        duration: 8000,
-        toValue: - Dimensions.get('window').width + 200,
-        useNativeDriver: false,
-      }),
-    ).start();
-
-    Animated.loop(
-      Animated.timing(coordinateYSecond, {
-        duration: 8000,
-        toValue: Dimensions.get('window').height - 300,
-        useNativeDriver: false,
-      }),
-    ).start();
-
-    Animated.loop(
-      Animated.timing(coordinateXThird, {
-        duration: 8000,
-        toValue: Dimensions.get('window').width - 200,
-        useNativeDriver: false,
-      }),
-    ).start();
-
-    Animated.loop(
-      Animated.timing(coordinateYThird, {
-        duration: 8000,
-        toValue: 0,
-        useNativeDriver: false,
-      }),
-    ).start();
-
-    Animated.loop(
-      Animated.timing(coordinateXFourth, {
-        duration: 8000,
-        toValue: 0,
-        useNativeDriver: false,
-      }),
-    ).start();
-
-    Animated.loop(
-      Animated.timing(coordinateYFourth, {
-        duration: 8000,
-        toValue: 0,
-        useNativeDriver: false,
-      }),
-    ).start();
-
-  }, []);
-
-  const interpolateItem =
-    sizeValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: ['0deg','90deg'],
+    const rotateText = spinValue.interpolate({
+        inputRange: [0, 0.2, 0.4, 0.6, 1],
+        outputRange: ['0deg', '720deg', '1220deg', '720deg', '0deg'],
     })
 
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Animated.View style={{ flexDirection: 'row' }}>
+    useEffect(() => {
+        Animated.loop(Animated.timing(textOpacity, {
+            toValue: 1,
+            duration: 3000,
+            useNativeDriver: false,
+        })).start();
+
+        Animated.loop(Animated.timing(textSize, {
+            toValue: 50,
+            duration: 3000,
+            useNativeDriver: false,
+        })).start();
+
+        Animated.loop(Animated.timing(spinValue, {
+            toValue: 1,
+            duration: 3000,
+            useNativeDriver: false,
+        })).start();
+
+        Animated.loop(Animated.timing(cordinateX, {
+            toValue: props.position.x,
+            duration: 3000,
+            useNativeDriver: false,
+        })).start();
+
+        Animated.loop(Animated.timing(cordinateY, {
+            toValue: props.position.y,
+            duration: 3000,
+            useNativeDriver: false,
+        }),).start();
+    })
+
+    return (
         <Animated.Text style={{
-          color: 'orange',
-          fontSize: sizeValue,
-          transform: [{ translateX: coordinateXFirst }, { translateY: coordinateYFirst }, { rotate: interpolateItem }],
-        }}>Text1</Animated.Text>
-      </Animated.View>
-      <Animated.View style={{ flexDirection: 'row' }}>
-        <Animated.Text style={{
-          color: 'orange',
-          fontSize: sizeValue,
-          right: 0,
-          position: 'absolute',
-          transform: [{ translateX: coordinateXSecond }, { translateY: coordinateYSecond }, { rotate: interpolateItem }],
-        }}>Text2</Animated.Text>
-      </Animated.View>
-      <Animated.View style={{ flexDirection: 'row' }}>
-        <Animated.Text style={{
-          color: 'orange',
-          fontSize: sizeValue,
-          position: 'absolute',
-          transform: [{ translateX: coordinateXThird }, { translateY: coordinateYThird }, { rotate: interpolateItem }],
-        }}>Text3</Animated.Text>
-      </Animated.View>
-      <Animated.View style={{ flexDirection: 'row' }}>
-        <Animated.Text style={{
-          color: 'orange',
-          fontSize: sizeValue,
-          position: 'absolute',
-          transform: [{ translateX: coordinateXFourth }, { translateY: coordinateYFourth }, { rotate: interpolateItem }],
-        }}>Text4</Animated.Text>
-      </Animated.View>
-    </SafeAreaView>
-  );
-};
+            color: 'red',
+            fontSize: 30,
+            top: props.top,
+            left: props.left,
+            right: props.right,
+            opacity: textOpacity,
+            bottom: props.bottom,
+            position: 'absolute',
+            transform: [{ translateX: cordinateX }, { translateY: cordinateY }, { rotate: rotateText },]
+        }}>Animation</Animated.Text>
+    )
+}
+
+export default nextAnimation = () => {
+    const { width, height } = Dimensions.get('window');
+    return (
+        <View style={styles.container}>
+            <Item position={{ x: width, y: height }} left={0} top={0} />
+            <Item position={{ x: -width, y: height }} right={0} top={0} />
+            <Item position={{ x: -width, y: -height }} right={0} bottom={0} />
+            <Item position={{ x: width, y: -height }} left={0} bottom={0} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
